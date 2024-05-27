c6bafa96c73556db13bcc188114409e13ccb0a0e
jasmineamber <jasmineamber@aliyun.com>
2024-05-27 23:56:31 +0800

    hifini: get cookie

--- Sign/hifini_cookie.js
+++ Sign/hifini_cookie.js
@@ -1,4 +1,6 @@
-const key = "hifini_cookies"
-$persistentStore.write(key, $response.headers["Set-Cookie"])
-$notification.post("title", $persistentStore.read(key))
-$done({})
+const key = "hifini_cookies";
+if ($request.method == "POST") {
+    $persistentStore.write(key, $response.headers["Set-Cookie"]);
+    $notification.post("title", $persistentStore.read(key));
+}
+$done({});
